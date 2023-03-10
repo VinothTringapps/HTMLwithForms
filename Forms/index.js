32b1bd008a1e61859c3a422763ea5cf12c690e85
vinothkumarp <123440655+VinothTringapps@users.noreply.github.com>
2023-03-10 10:52:43 +0530

    Update index.js

--- Forms/index.js
+++ Forms/index.js
@@ -11,18 +11,21 @@ if(Name=="" || Name.length<4){
     document.getElementById("span").innerHTML="please Enter the Correct Name";
     return false;
 }
- if(phno=="" || phno.length<10){
+ if(phno=="" || phno.length==10){
     document.getElementById("span").innerHTML="Please enter the Correct Phone Number";
     return false;
 }
- if(address=="" || address.length<10){
-    document.getElementById("span").innerHTML="Please enter the Correct Address"
-    return false;
-}
+    
 if(date==""){
     document.getElementById("span").innerHTML="Please enter the Date";
     return false;
 }
+    
+ if(address=="" || address.length<10){
+    document.getElementById("span").innerHTML="Please enter the Correct Address"
+    return false;
+}
+
 else{
     document.getElementById("span").innerHTML=" ";
     console.log("Successfull");
